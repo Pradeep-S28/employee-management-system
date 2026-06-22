623e53609a467d45e17c76b109f365aa97bc37f3
Pradeep-S28 <emails2pradeep1506@gmail.com>
2026-06-22 22:18:45 +0530

    Connect backend to MySQL

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Employee Management API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
